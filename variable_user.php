fc38c74939e0bdbe807295f936dacf441b95b52c
Siti Roudotul Khoiriah <sitiroudotul5@gmail.com>
2025-03-02 16:12:12 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+//mendefinisikan variable
+$nama = 'Siti Roudotul';
+$umur = 20;
+$berat = 51.5;
+
+
+echo 'nama saya ' . $nama;
+echo '<br/>umur : ' . $umur . ' tahun';
+echo '<br/>berat : ' . $berat;
+?>
